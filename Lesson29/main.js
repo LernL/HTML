71dd3afa632034b14f35cb90e22d7e0f08180269
LernL <lernandlord@gmail.com>
2021-02-18 21:33:27 +0200

    Projects

---
+++ Lesson29/main.js
@@ -0,0 +1,22 @@
+// let x = 10
+//     y = 10 
+//     z = 10 
+//     c = x + y + z
+
+//     console.log(c)
+let myName = "Tima"
+    myAge = 15
+    userName = prompt("What your name?")
+    userAge = prompt("How old are you?")
+    userPlace = prompt("Where you from?")
+    userInf = prompt("How are you?")
+    userInt = prompt("What you like do?")
+    
+    console.log(myName, "-",myAge)
+    console.log(userName,userAge,userPlace,userInf,userInt)
+
+    alert("Hello.");
+    alert("Hello world.");
+    alert("Hello ," + (userName))
+    alert("You are the best!")
+    alert("But you can do more")
---
+++ Lesson31(H.W)/main.js
@@ -0,0 +1,16 @@
+let x = Number(prompt("Введите первое число"))
+    y = Number(prompt("Введите второе число"))
+    alert("Сумма: "+(x+y))
+    alert("Разница: "+(x-y))
+    alert("Произведение: "+(x*y))
+    alert("Деление: "+(x/y))
+    alert("Воспроизведение в степень: "+(x**y))
+    alert("Остача: "+(x%y))
+    alert("Равны ли они?: "+(x==y))
+    console.log(x+y);
+    console.log(x-y);
+    console.log(x*y);
+    console.log(x/y);
+    console.log(x**y);
+    console.log(y%x);
+    console.log(x==y);
