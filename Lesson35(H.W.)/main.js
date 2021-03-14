dfd00bd4c0f6091f69da01a6e2b83d2008ad0ac4
LernL <lernandlord@gmail.com>
2021-03-14 16:55:06 +0200

    Project

---
+++ Lesson35(H.W.)/main.js
@@ -0,0 +1,15 @@
+let i = 0
+
+while(i<1000){
+    if(i%5==0){
+        console.log(i);
+        i++
+    }
+    i++
+}
+
+let arr=[1,2,3,4,5,6,7,8,9,10]
+for(i=0;i<10;i++){
+    if(i%3==0){
+    console.log(i)}
+}
