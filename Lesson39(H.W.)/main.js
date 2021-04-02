005e8a30808e1997542cd2b0e3a8213178145367
LernL <lernandlord@gmail.com>
2021-04-02 15:00:35 +0300

    Project

---
+++ Lesson39(H.W.)/main.js
@@ -0,0 +1,60 @@
+let a= +prompt("a=")
+
+if (a==0){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT1
+let b= +prompt("b=")
+if (b>0){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT2
+let c= +prompt("c=")
+if (c<0){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT3
+let d= +prompt("d=")
+if (d>=0){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT4
+let e= +prompt("e=")
+if (e<=0){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT5
+let n= +prompt("n=")
+if (n!=0){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT6
+let f= prompt("f=")
+if (f=="test"){
+    alert("Верно");
+}
+else{
+    alert("Неверно");
+}//IT7
+let t= prompt("t=")
+if (t=="1"){
+    alert("Верно");
+}
+else if (t=='"1"'){
+    alert("Верно");
+}
+else{
+    alert("Неверно")
+}//IT8
--- Lesson39/main.js
+++ Lesson39/main.js
@@ -1,59 +1,59 @@
-// let a= +prompt("a=")
+let a= +prompt("a=")
 
-// switch(a){
-//     case 0:
-//         alert("Верно");
-//     default:
-//         alert("Неверно")
-// }//IT1
-// let b= +prompt("b=")
-// switch(b>0){
-//     case true:
-//         alert("Верно")
-//         break;
-//     default:
-//         alert("Неверно");
-// }//IT2
-// let c= +prompt("c=")
-// switch(c<0){
-//     case true:
-//         alert("Верно")
-//         break;
-//     default:
-//         alert("Неверно");
-// }//IT3
-// let d= +prompt("d=")
-// switch(d>=0){
-//     case true:
-//         alert("Верно")
-//         break;
-//     default:
-//         alert("Неверно");
-// }//IT4
-// let e= +prompt("e=")
-// switch(e<=0){
-//     case true:
-//         alert("Верно")
-//         break;
-//     default:
-//         alert("Неверно");
-// }//IT5
-// let n= +prompt("n=")
-// switch(n!=0){
-//     case true:
-//         alert("Верно")
-//         break;
-//     default:
-//         alert("Неверно");
-// }//IT6
-// let f= prompt("f=")
-// switch(f){
-//     case "test":
-//         alert("Верно")
-//         break;
-//     default:
-//         alert("Неверно");
-// }//IT7
+switch(a){
+    case 0:
+        alert("Верно");
+    default:
+        alert("Неверно")
+}//IT1
+let b= +prompt("b=")
+switch(b>0){
+    case true:
+        alert("Верно")
+        break;
+    default:
+        alert("Неверно");
+}//IT2
+let c= +prompt("c=")
+switch(c<0){
+    case true:
+        alert("Верно")
+        break;
+    default:
+        alert("Неверно");
+}//IT3
+let d= +prompt("d=")
+switch(d>=0){
+    case true:
+        alert("Верно")
+        break;
+    default:
+        alert("Неверно");
+}//IT4
+let e= +prompt("e=")
+switch(e<=0){
+    case true:
+        alert("Верно")
+        break;
+    default:
+        alert("Неверно");
+}//IT5
+let n= +prompt("n=")
+switch(n!=0){
+    case true:
+        alert("Верно")
+        break;
+    default:
+        alert("Неверно");
+}//IT6
+let f= prompt("f=")
+switch(f){
+    case "test":
+        alert("Верно")
+        break;
+    default:
+        alert("Неверно");
+}//IT7
 let t= prompt("t=")
 switch(t){
     case "1":
